4b25a38f91a95a59711c7d76e8b617fcd40906c6
𝒮𝑒𝓋𝒾𝓃𝒸𝒽 <ysevinchoy2197@gmail.com>
2026-01-08 14:40:17 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,31 @@
+#1-misol
+lst = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(lst[:10])
+
+#2-misol
+lst = [1, 2, 3, 4, 5]
+
+new_list = lst[:1]
+
+print(new_list)
+
+
+#3-misol
+lst = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+last_list = lst[-1:]
+print(last_list)
+
+
+#4-misol
+lst = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+middle = lst[3:6]
+print(middle)
+
+
+#5-misol
+lst = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+first_two = lst[:2]
+last_two = lst[-2:]
+
+print(first_two)
+print(last_two)
